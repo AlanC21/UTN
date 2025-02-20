c6034f5f472b0dfa825567332bd2e90d6a65535b
Alan Cordoba <105000699+AlanC21@users.noreply.github.com>
2025-02-20 08:38:12 -0300

    Add files via upload

---
+++ TP 1/Ejercicio10/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2, num3;
+
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &num1);
+
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &num2);
+
+    printf("Ingrese el tercer numero: ");
+    scanf("%d", &num3);
+
+    if(num1 > num2 && num1 > num3) {
+        printf("El numero mas grande es: %d", num1);
+    } else if(num2 > num1 && num2 > num3) {
+        printf("El numero mas grande es: %d", num2);
+    } else {
+        printf("El numero mas grande es: %d", num3);
+    }
+
+    return 0;
+}
